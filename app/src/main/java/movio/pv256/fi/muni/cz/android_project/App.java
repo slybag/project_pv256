8b85825892aac40d05bb1bd905ad0fe0c7a48b61
slybag <rais.mic@gmail.com>
2015-10-05 08:39:30 +0200

    Starting project, add init strict mode

---
+++ app/src/main/java/movio/pv256/fi/muni/cz/android_project/App.java
@@ -0,0 +1,38 @@
+package movio.pv256.fi.muni.cz.android_project;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+
+/**
+ * Created by Michal on 5. 10. 2015.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            initStrictMode();
+        }
+    }
+
+    private void initStrictMode() {
+        StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            tpb.penaltyFlashScreen();
+        }
+        StrictMode.setThreadPolicy(tpb.build());
+
+        StrictMode.VmPolicy.Builder vmpb = new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            vmpb.detectLeakedClosableObjects();
+        }
+        StrictMode.setVmPolicy(vmpb.build());
+    }
+}
